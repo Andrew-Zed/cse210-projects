eb14a11dd2e2c3e33ae2e1eda34c058a52e99637
Andrew_Awadike <andrew.awadike@moniepoint.com>
2025-03-05 22:06:56 +0100

    feat: Implement basic grading system in C#

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int percentage = int.Parse(response);
+
+        string grade = "";
+
+        if(percentage >= 90)
+        {
+            grade = "A";
+        } 
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60) 
+        {
+            grade = "D";
+        } else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if(percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        } 
+        else
+        {
+            Console.WriteLine("Better Luck next time!");
+        }
     }
 }
