2439b15fa9e350b66462e2a236316628184bef0f
Andrew_Awadike <andrew.awadike@moniepoint.com>
2025-03-05 15:43:47 +0100

    feat: Add user input prompt for first and last name with formatted output

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello Andrew Welcome to CSE210");
+        Console.Write("What is your name ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
     }
 }
