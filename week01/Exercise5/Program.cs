2f7f3fcede2b4b09b03bb6c9bdc4c9787bdd25f6
Andrew_Awadike <andrew.awadike@moniepoint.com>
2025-03-07 20:49:38 +0100

    Added user interaction program to square a favorite number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        string userName = PromptUserName();
+        int favouriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favouriteNumber);
+
+        DisplayResult(userName, squaredNumber);
+
     }
+    
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return (number * number);
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"Brother {userName}, the square of your number is {square}");
+    }
+
+
 }
