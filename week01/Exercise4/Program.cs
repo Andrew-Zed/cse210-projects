abacefb6f30a02dc0662df7113559416289f0acc
Andrew_Awadike <andrew.awadike@moniepoint.com>
2025-03-07 19:43:05 +0100

    implemented number collection, sum, average, and max calculation

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+
+        }
+        while (userNumber != 0);
+        {
+
+            if (numbers.Count > 0)
+            {
+                int sum = 0;
+                foreach (int number in numbers)
+                {
+                    sum += number;
+                }
+
+                Console.WriteLine($"The sum is: {sum}");
+
+                float average = ((float)sum) / numbers.Count;
+                Console.WriteLine($"The average is: {average}");
+
+                int maxNumber = numbers[0];
+
+                foreach (var number in numbers)
+                {
+                    if (number > maxNumber)
+                    {
+                        maxNumber = number;
+                    }
+                }
+
+                Console.WriteLine($"The max number: {maxNumber}");
+
+            }
+
+        }
+
     }
 }
