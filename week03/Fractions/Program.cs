b4560572c24aab56677216e4b35d1e6137a77d9f
Andrew_Awadike <andrew.awadike@moniepoint.com>
2025-03-30 17:28:40 +0100

    Implemented Fraction class with constructors, getters, setters, and methods for fractional and decimal representations. Added validation for zero denominator. Updated Program.cs to test the class functionality.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Console.WriteLine("Hello World! This is the Fractions Project.\n");
+
+        Fraction defaultFraction = new Fraction();
+        Fraction wholeNumberFraction = new Fraction(5);
+        Fraction customFraction = new Fraction(3, 4);
+
+        Console.WriteLine("Default: " + defaultFraction.GetFractionString());
+        Console.WriteLine("Decimal: " + defaultFraction.GetDecimalValue());
+        
+        Console.WriteLine("Whole Number: " + wholeNumberFraction.GetFractionString());
+        Console.WriteLine("Decimal: " + wholeNumberFraction.GetDecimalValue());
+
+        Console.WriteLine("Custom: " + customFraction.GetFractionString());
+        Console.WriteLine("Decimal: " + customFraction.GetDecimalValue());
+
+        // Testing Getters and Setters
+        customFraction.SetTop(1);
+        customFraction.SetBottom(3);
+        Console.WriteLine("Updated Fraction: " + customFraction.GetFractionString());
+        Console.WriteLine("Update Decimal: " + customFraction.GetDecimalValue()); 
+        
+
     }
 }
